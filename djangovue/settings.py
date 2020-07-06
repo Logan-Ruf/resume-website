3401ec3a4a8a752f1d28d4f5fd9b825ce84108f2
Logan Ruf <cruf3@wgu.edu>
2020-07-05 20:55:56 -0700

    Added docker

--- djangovue/settings.py
+++ djangovue/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = SECURITY_KEY
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'djangovue.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'db',
+        'PORT': 5432,
     }
 }
 
