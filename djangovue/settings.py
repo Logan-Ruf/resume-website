4445e3a9a832f4c6772abab798c7b77a711787ee
Logan Ruf <cruf3@wgu.edu>
2020-07-05 20:33:37 -0700

    Finished layout for SPA

--- djangovue/settings.py
+++ djangovue/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+from secretkey import SECURITY_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '$q27lf$vyo6n^*5v(xfuzsns0s9#s3kvn01z4y94xpkgn&mgx#'
+SECRET_KEY = SECURITY_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
